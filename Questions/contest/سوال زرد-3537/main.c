91c89c8e62d348553422c173483c99ef223e8702
kasrazarei39 <kasrazarei39@gmail.com>
2021-03-02 20:33:27 +0330

    Adding problem 3537

---
+++ Questions/contest/سوال زرد-3537/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int i=0;
+    scanf("%d",&a);
+    printf("W");
+    while(i<a)
+    {
+        printf("o");
+        i++;
+    }
+    printf("w!");
+    return 0;
+}
